36c7d52d6c816a6afd06873ee067a40cb7a3e3a3
mvraa <40834090+mvraa@users.noreply.github.com>
2020-10-22 17:20:43 +0200

    kommentarer til ex7

--- UDP/server/file_server.py
+++ UDP/server/file_server.py
@@ -12,17 +12,18 @@ FORMAT = 'utf-4'
 
 def main(argv):
 	print("Server set to ", ADDR)
-	server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	server.bind(ADDR)
+	server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # setup server som ipv4 og UDP
+	server.bind(ADDR) # find adresse til server
 
 	while 1:
 		print("Server er klar.")
-		bytesAddrPair = server.recvfrom(HEADER)
-		msg = bytesAddrPair[0]
-		clientAddr = bytesAddrPair[1]
-		print("Message received", clientAddr, ":", msg.decode())
-		server.sendto(msg, clientAddr)
+		bytesAddrPair = server.recvfrom(HEADER) # modtag besked fra en klient
+		msg = bytesAddrPair[0] # index 0 indeholder selve besked
+		clientAddr = bytesAddrPair[1] # index 1 indeholder client addressen
+		print("Message received", clientAddr, ":", msg.decode()) # print addresse samt besked
+		server.sendto(msg, clientAddr) # send besked retur som verification. klient addresse specificeres nu som argument
 
+		# hvis besked er 'u' sættes fil til uptime og hvis den er 'l' sættes den til loadavg
 		if msg.decode() == 'U' or msg.decode() == 'u':
 			fileMsg = "/proc/uptime"
 		elif msg.decode() == 'L' or msg.decode() == 'l':
@@ -30,20 +31,22 @@ def main(argv):
 		
 		print("File to send:", fileMsg)
 
-		sendFile(fileMsg, clientAddr, server) # send file
+		sendFile(fileMsg, clientAddr, server) # funktion der sender fil. client addresse og server er argumenter, da funktion skal vide hvem der sender/modtager
 
 
 def sendFile(fileName, clientAddr, server):
 	try:
+		# send size af fil
 		msg = "File size: " + str(Lib.check_File_Exists(fileName))
 		server.sendto(msg.encode(), clientAddr)
 	except:
+		# send fejl besked
 		server.sendto("File not found!".encode(), clientAddr)
 
-	with open(fileName, "rb") as file:
-		data = file.read(HEADER)
+	with open(fileName, "rb") as file: # læs fra filen
+		data = file.read(HEADER) # læs første 1000 bytes
 		print("Sending...")
-		while data:
+		while data: # send mens der er data tilbage
 			server.sendto(data, clientAddr)
 			data = file.read(HEADER)
 	print("File sent.")
